76aafd89e20acd2a922abd29fe17c1622ec7b722
titohajili <maqatoto963@gmail.com>
2025-04-07 13:02:26 +0400

    Component Sidebar

--- deepseek/app/page.jsx
+++ deepseek/app/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { assets } from "@/assets/assets";
+import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -12,7 +13,7 @@ export default function Home() {
   return (
     <div>
       <div className="flex h-screen">
-        {/* -- sidebar */}
+        <Sidebar expand={expand} setExpand={setExpand} />
         <div className="flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative">
           <div className="md:hidden absolute px-4 top-6 flex items-center justify-between w-full">
             <Image onClick={()=>(expand ? setExpand(false) : setExpand(true))} className="rotate-180" src={assets.menu_icon} alt=""/>
---
+++ deepseek/components/Sidebar.jsx
@@ -0,0 +1,27 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Sidebar = ({expand, setExpand}) => {
+  return (
+    <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
+      <div>
+        <div className={`flex ${expand ? "flex-row gap-10" : "flex-col items-center gap-8"}`}>
+            <Image className={expand ? "w-36": "w-10"} src={expand ? assets.logo_text : assets.logo_icon} alt=''/>
+
+            <div onClick={()=>(expand ? setExpand(false) : setExpand(true))}
+             className='group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer'>
+                <Image src={assets.menu_icon} alt='' className='md:hidden' />
+                <Image src={expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt='' className='hidden md:block w-7' />
+                <div>
+                    {expand ? 'Close sidebar' : 'Open sidebar'}
+                    <div className={`w-3 h-3 absolute bg-black rotate-45 ${expand ? "left-1/2 top-1.5 -translate-x-1/2" : "left-4 bottom-1.5"}`}></div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
