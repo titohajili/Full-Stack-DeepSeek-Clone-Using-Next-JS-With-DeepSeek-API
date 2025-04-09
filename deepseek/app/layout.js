ccbf438a77209ccca2950786df1fc07d0ad32b2f
titohajili <maqatoto963@gmail.com>
2025-04-09 18:18:54 +0400

    App Context

--- deepseek/app/layout.js
+++ deepseek/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { AppContextProvider } from "@/context/AppContext";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -15,15 +16,16 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
+      <AppContextProvider>
 
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-    
+        <html lang="en">
+          <body
+            className={`${inter.className} antialiased`}
+          >
+            {children}
+          </body>
+        </html>
+      </AppContextProvider>
     </ClerkProvider>
   );
 }
--- deepseek/components/Sidebar.jsx
+++ deepseek/components/Sidebar.jsx
@@ -1,8 +1,14 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
+import { useClerk, UserButton } from '@clerk/nextjs'
+import { useAppContext } from '@/context/AppContext'
 
 const Sidebar = ({expand, setExpand}) => {
+
+  const {openSignIn} = useClerk();
+  const {user} = useAppContext();
+
   return (
     <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'p-4 w-64' : 'md:w-20 w-0 max-md:overflow-hidden'}`}>
       <div>
@@ -72,8 +78,11 @@ const Sidebar = ({expand, setExpand}) => {
           {expand && <><span>Get App</span> <Image src={assets.new_icon} alt='' /> </>}
         </div>
 
-      <div className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
-        <Image src={assets.profile_icon} alt='' className='w-7' />
+      <div onClick={user ? null : openSignIn}
+       className={`flex items-center ${expand ? 'hover:bg-white/10 rounded-lg' : 'justify-center w-full'} gap-3 text-white/60 text-sm p-2 mt-2 cursor-pointer`}>
+        {
+          user ? <UserButton/> : <Image src={assets.profile_icon} alt='' className='w-7' /> 
+        }
         {expand && <span>My Profile</span>}
       </div>
 
