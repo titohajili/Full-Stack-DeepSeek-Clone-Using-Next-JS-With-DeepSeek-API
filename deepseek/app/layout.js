5b828a654376ffa9a96f9258bf83b54e1586ce42
titohajili <maqatoto963@gmail.com>
2025-04-09 17:24:48 +0400

    ClerkProvider

--- deepseek/app/layout.js
+++ deepseek/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -13,6 +14,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body
         className={`${inter.className} antialiased`}
@@ -20,5 +23,7 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    
+    </ClerkProvider>
   );
 }
