b873710285f1ffeecdec06a473d1bb1e8eee8956
titohajili <maqatoto963@gmail.com>
2025-04-22 17:23:01 +0400

    last push

--- deepseek/config/db.js
+++ deepseek/config/db.js
@@ -5,9 +5,7 @@ let cached = global.mongoose || {conn: null, promise: null};
 export default async function connectDB(){
     if(cached.conn) return cached.conn;
     if(!cached.promise){
-        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) => {
-            return mongoose;
-        })
+        cached.promise = mongoose.connect(process.env.MONGODB_URI).then((mongoose) =>  mongoose );
     }
     try{
         cached.conn = await cached.promise;
